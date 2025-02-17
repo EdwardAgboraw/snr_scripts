0393ac2047a84ec57ac6f7ad0344ebba030a1618
EdwardAgboraw <edward.agboraw@gmail.com>
2025-02-17 14:37:29 +0000

    finalised extract_soma() function, adapted check_soma script

--- snr_functions.py
+++ snr_functions.py
@@ -8,15 +8,37 @@ Library of custom functions created for the analysis and visualization
 of single-neuron reconstruction data
 """
 
-def extract_soma(data_path):
+import pandas as pd
+import numpy as np
+
+def extract_soma(data_path, num_files):
     
     #open data directory
     #extra soma information from all _trans.csv files
     #save soma coordiantes to list of lists
     #convert into numpy float32 array
     
+    upper_lim = num_files + 1
     
+    all_soma = []
     
+    #get all soma coordinates
+    for x in range(1,upper_lim):
+        
+        file_name = str(x) + "_trans.csv"
+        
+        file_path = data_path + file_name
+        
+        n = pd.read_csv(file_path, nrows = 1)
+        
+        #convert to 25x25x25 resolution
+        soma_coords = [n["x"][0] * 25,
+                       n["y"][0] * 25,
+                       n["z"][0] * 25]
     
+        all_soma.append(soma_coords)
+        
+    #convert to numpy float 32 array
+    all_soma = np.asarray(all_soma, dtype=np.float32)
     
-    return
+    return(all_soma)
